979cdeca6091fdd52d811d8f668620cdb088276f
liujin <liujin@xunyou.com>
2020-07-30 05:30:23 +0800

    update

---
+++ config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "迅游加速器，支持PC和主机游戏加速！", 
+    "home_url": "Module_xunyou.asp", 
+    "md5": "e122e97499b81b73ec0b3fe75d87570a", 
+    "module": "xunyou", 
+    "title": "迅游加速器", 
+    "version": "1.0.0.7"
+}
