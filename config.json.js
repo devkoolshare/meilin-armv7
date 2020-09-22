2bf01292cba076f1bdc9a09312a581f07088ec49
Moyision <entanglement@139.com>
2020-09-22 16:16:56 +0800

    更新内容： 1、devType移动至xunyou/config 2、卸载插件时，同时删除devType

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "迅游加速器，支持PC和主机游戏加速！", 
     "home_url": "Module_xunyou.asp", 
-    "md5": "6949cf58309def18ac842b420497d617", 
+    "md5": "f59f0b410cb22a283a7af70aa9b1724d", 
     "module": "xunyou", 
     "title": "迅游加速器", 
-    "version": "1.0.0.10"
+    "version": "1.0.0.11"
 }
