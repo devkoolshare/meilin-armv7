6e3616c1a059c76165b9cb2e5422f0e1db4cde7d
Moyision <entanglement@139.com>
2020-10-30 20:31:09 +0800

    更新内容： 1、新增IPset应用 2、增加体验功能

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "迅游加速器，支持PC和主机游戏加速！", 
     "home_url": "Module_xunyou.asp", 
-    "md5": "f59f0b410cb22a283a7af70aa9b1724d", 
+    "md5": "fbbb90776b55286e26c2514c59d2e051", 
     "module": "xunyou", 
     "title": "迅游加速器", 
-    "version": "1.0.0.11"
+    "version": "1.0.0.13"
 }
