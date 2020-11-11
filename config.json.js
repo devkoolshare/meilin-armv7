45037599f67ce9f5c1800e7fff134d4a81051ae4
Moyision <entanglement@139.com>
2020-11-11 12:13:28 +0800

    错误提示优化

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "迅游加速器，支持PC和主机游戏加速！", 
     "home_url": "Module_xunyou.asp", 
-    "md5": "fbbb90776b55286e26c2514c59d2e051", 
+    "md5": "688136ade6f121356fdb390403069347", 
     "module": "xunyou", 
     "title": "迅游加速器", 
-    "version": "1.0.0.13"
+    "version": "1.0.0.14"
 }
