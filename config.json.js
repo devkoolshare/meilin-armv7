94b023f8d1c1b6c91942c75ebaf72c8fde056ea7
devkoolshare <devkoolshare@xunyou.com>
2020-12-14 15:47:37 +0800

    1.优化测速机制 2.增加弱网环境加速保活机制

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "迅游加速器，支持PC和主机游戏加速！", 
     "home_url": "Module_xunyou.asp", 
-    "md5": "ecf2a598731fa3500044d38f4df014e8", 
+    "md5": "8183FA319181C820FDC6304E7F840D3D", 
     "module": "xunyou", 
     "title": "迅游加速器", 
-    "version": "1.0.0.15"
+    "version": "1.0.1.2"
 }
