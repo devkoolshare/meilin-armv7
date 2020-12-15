445b74c36484bfc44b4b3d24b20684fdd2e98c70
devkoolshare <devkoolshare@xunyou.com>
2020-12-15 18:57:43 +0800

    更新版本号

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "迅游加速器，支持PC和主机游戏加速！", 
     "home_url": "Module_xunyou.asp", 
-    "md5": "8183fa319181c820fdc6304e7f840d3d", 
+    "md5": "87df2fa1b94e594c45e835cd954aea34", 
     "module": "xunyou", 
     "title": "迅游加速器", 
-    "version": "1.0.1.3"
+    "version": "1.0.1.4"
 }
