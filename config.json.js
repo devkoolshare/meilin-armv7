984f14df751a854c3aac02567b45ba113c9f1b8b
moyision <entanglement@139.com>
2021-04-23 12:10:22 +0800

    更新版本号

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "迅游加速器，支持PC和主机游戏加速！", 
     "home_url": "Module_xunyou.asp", 
-    "md5": "923C38475A9B8D286495764C87F3E956", 
+    "md5": "CC7F839A256BF1EDDDDC87008F71BD9E", 
     "module": "xunyou", 
     "title": "迅游加速器", 
-    "version": "1.0.5.0"
+    "version": "1.0.5.4"
 }
