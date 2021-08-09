9a1c656b01909e7ecaf81e62c9e8f440d18d694e
xunyou <devkoolshare@xunyou.com>
2021-08-09 14:36:41 +0800

    更新版本号至1.0.9.1

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "迅游加速器，支持PC和主机游戏加速！", 
     "home_url": "Module_xunyou.asp", 
-    "md5": "BC96703A23042842B4D03C80C27EA7F5", 
+    "md5": "96E089AE4AA047C291ECB5CB649D6547", 
     "module": "xunyou", 
     "title": "迅游加速器", 
-    "version": "1.0.9.0"
+    "version": "1.0.9.1"
 }
